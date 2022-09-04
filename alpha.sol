e56bf61811087f60ed29a1d71826e37a13a8d80b
axe10ten <67701676+axe10ten@users.noreply.github.com>
2022-09-03 23:19:38 -0500

    Update alpha.sol

--- alpha.sol
+++ alpha.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Axe10ten is ERC721Enumerable, Ownable {
+contract FBN is ERC721Enumerable, Ownable {
 
 struct TokenInfo {
         IERC20 paytoken;
